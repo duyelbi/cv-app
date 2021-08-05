921ff57f997921e7b9f8be8697904c1bf3de04e0
Nguyen Duc Duy <39079556+duyelbi@users.noreply.github.com>
2021-08-05 15:33:26 +0700

    first commit

---
+++ src/layouts/header/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function header() {
+  return (
+    <header className="container">
+      <h1 className="test">header</h1>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a className="navbar-brand" href="#">
+          RectCV
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default header;
