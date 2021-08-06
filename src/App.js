17b6de33ce298f0ccf29bbc6e9952b70e97903bb
Nguyen Duc Duy <39079556+duyelbi@users.noreply.github.com>
2021-08-06 09:46:07 +0700

    complete home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+
+import Particles from 'react-particles-js';
+
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./layouts/header/header";
 
 function App() {
   return (
     <div className="App">
+      <Particles className="paticles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width:6,
+                  color: "#ffffff"
+                }
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
       <Header />
     </div>
