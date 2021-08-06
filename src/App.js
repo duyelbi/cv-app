b98687044a06fb2121762c8812032f2dd0435662
Nguyen Duc Duy <39079556+duyelbi@users.noreply.github.com>
2021-08-06 09:05:03 +0700

    firt header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./layouts/header/header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import Typed from "react-typed";
 
-function header() {
+function Header() {
   return (
-    <header className="container">
-      <h1 className="test">header</h1>
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a className="navbar-brand" href="#">
-          RectCV
-        </a>
-      </nav>
+    <header className="header-wraper">
+      <div className="main-info">
+        <Typed
+          className="typed-text"
+          strings={["Web Design", "Web Development"]}
+          typedSpeed={80}
+          backSpeed= {120}
+          loop
+        />
+        <h1 className="test">Duy Elbi</h1>
+        <a href="#" className="btn-main-offer">Learn More</a>
+      </div>
     </header>
   );
 }
 
-export default header;
+export default Header;
